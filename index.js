40d5a71251722e5ce556c3c847fc8d045f8b48a0
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-03-05 23:21:07 +0100

    Answer questions from readMe

---
+++ index.js
@@ -0,0 +1,68 @@
+// QUESTION TWO
+const languages = ["C#", "JavaScript", "Ruby", "PHP", "Python"];
+
+// Add ‘Kotlin’ to the end of the array
+languages.push("Kotlin");
+// console.log(languages);
+
+// Add ‘Java’ after ‘Ruby’
+languages.splice(3, 0, "Java");
+// console.log(languages);
+
+// Remove the first item in the array
+languages.shift();
+// console.log(languages);
+
+// Add ’Scala’ and ‘Swift’ to the beginning of the array
+languages.unshift("Scala", "Swift");
+// console.log(languages);
+
+// Replace ‘PHP’ with ‘Go’ and ‘Rust’
+languages.splice(3, 1, "Go", "Rust");
+// console.log(languages);
+
+//  QUESTION THREE
+let fruit = ["apple", "mango", "banana"];
+function changeFruit(fruit) {
+  fruit[2] = "orange";
+  return fruit;
+}
+
+console.log(fruit);
+
+// QUESTION FOUR
+/**
+ * declare a function called maxValue that takes an array of numbers called numsArray
+ * initialize the maximum value with 0
+ * loop through the array to check if a given index is greater than the max value,
+ * if it is, assign the value back to the maximum value variable
+ * return the maximum
+ *
+ */
+
+const maxValueArray = (numsArray) => {
+  let maxValue = 0;
+  for (let i = 0; i < numsArray.length - 1; i++) {
+    if (numsArray[i] > maxValue) {
+      maxValue = numsArray[i];
+    }
+  }
+  return maxValue;
+};
+console.log(maxValueArray([10, 5, 10, -2]));
+
+// QUESTION FIVE
+/**
+ * declare a function called valTimesIndex
+ * using the map method, return a new array with each value multiplied by the index
+ */
+// const valTimesIndex = (numsArray) => {
+//   numsArray.map((value, index) => {
+//     return value * index;
+//   });
+// };
+
+const valTimesIndex = (numsArray) =>
+  numsArray.map((value, index) => value * index);
+
+console.log(valTimesIndex([1, 2, 3]));
